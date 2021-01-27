85eb8a10392602fec6e64c0c8f59817705e908e9
almohelo <almoyner.heloise@gmail.com>
2021-01-27 22:00:11 +0100

    ajout commentaires controllers/user.js

--- controllers/user.js
+++ controllers/user.js
@@ -1,18 +1,18 @@
 const User = require('../models/User');
 const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken');            
 const sanitize = require('mongo-sanitize');
 
+//requête inscription
 exports.signup = (req, res, next) => {
-
-    const email = sanitize(req.body.email);
+    const email = sanitize(req.body.email);         //nettoie les entrées 
     const password = sanitize(req.body.password);
 
     bcrypt.hash(password, 10)
         .then(hash => {
             const user = new User({
                 email: email,
-                password: hash
+                password: hash                      //hashage du mot de passe   
             });
             user.save()
                 .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
@@ -21,8 +21,8 @@ exports.signup = (req, res, next) => {
         .catch(error => res.status(500).json({ error }));
 };
 
+//requête identification
 exports.login = (req, res, next) => {
-
     const email = sanitize(req.body.email);
     const password = sanitize(req.body.password);
 
@@ -40,8 +40,8 @@ exports.login = (req, res, next) => {
                         userId: user._id,
                         token: jwt.sign(
                             { userId: user._id },
-                            'RANDOM_TOKEN_SECRET',
-                            { expiresIn: '2h' }
+                            'RANDOM_TOKEN_SECRET',      //chaîne de developpement temporaire pour encoder token
+                            { expiresIn: '2h' }         //durée de validité du token
                         )
                     });
                 })
